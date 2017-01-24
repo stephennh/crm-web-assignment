a7b7fb7b71e1c9ee5aaed785afa7fed6017a1a91
Stephen H <stephen210huang@hotmail.com>
2017-01-24 13:16:56 -0500

    contacts.erb updated with accessing names, crm.rb fake profiles created

--- crm.rb
+++ crm.rb
@@ -1,6 +1,7 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative "contact"
 require "sinatra"
 
 get "/" do
@@ -9,5 +10,5 @@ get "/" do
 end
 
 get "/contacts" do
-  
+
 end
