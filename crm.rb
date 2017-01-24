a77422de2d82803f94123a9f35cb858c802be5ca
Stephen H <stephen210huang@hotmail.com>
2017-01-24 13:20:19 -0500

    new_contact.erb created

--- crm.rb
+++ crm.rb
@@ -1,8 +1,14 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
-require_relative "contact"
+require_relative "contacts"
+require_relative "new_contact"
 require "sinatra"
+
+Contact.create("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+Contact.create("Steve", "Jobs", "steve@apple.com", "Co-Founder")
+Contact.create("Bill", "Gates", "bill@microsoft.com", "CEO")
+
 
 get "/" do
   @crm_app_name = "Stephen's CRM"
@@ -10,5 +16,9 @@ get "/" do
 end
 
 get "/contacts" do
+
+end
+
+get "/contacts/new" do
 
 end
