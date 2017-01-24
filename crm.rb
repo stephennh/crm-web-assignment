60f732d03ccaf4e8f996f990b9c72673598800d8
Stephen H <stephen210huang@hotmail.com>
2017-01-24 13:51:37 -0500

    crm.rb fully functioning

--- crm.rb
+++ crm.rb
@@ -4,9 +4,9 @@
 require_relative "contact"
 require "sinatra"
 
-Contact.create("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-Contact.create("Steve", "Jobs", "steve@apple.com", "Co-Founder")
-Contact.create("Bill", "Gates", "bill@microsoft.com", "CEO")
+# Contact.create("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+# Contact.create("Steve", "Jobs", "steve@apple.com", "Co-Founder")
+# Contact.create("Bill", "Gates", "bill@microsoft.com", "CEO")
 
 
 get "/" do
@@ -20,4 +20,11 @@ end
 
 get "/contacts/new" do
   return erb(:new_contact)
+end
+
+post "/contacts" do
+  #creates new contact from user input
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  # redirects you back to /contacts page
+  redirect to ("/contacts")
 end
