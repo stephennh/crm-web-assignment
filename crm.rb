f5e58c82f30fcaf7367fb498d4564314bf5c6035
Stephen H <stephen210huang@hotmail.com>
2017-01-26 15:17:38 -0500

    view/edit/delete all working

--- crm.rb
+++ crm.rb
@@ -8,46 +8,43 @@ require "sinatra"
 # Contact.create("Steve", "Jobs", "steve@apple.com", "Co-Founder")
 # Contact.create("Bill", "Gates", "bill@microsoft.com", "CEO")
 
-
+# --------Home Page---------
 get "/" do
   @crm_app_name = "Stephen's CRM"
   return erb(:index)
 end
 
-
+# ---------Contacts Home Page----------
 get "/contacts" do
   @count = Contact.all.count
   #links to contacts.erb file
   return erb(:contacts)
 end
 
-
+# --------Create a New Contact Page----------
 get "/contacts/new" do
   #links to new_contact.erb
   return erb(:new_contact)
 end
 
-
+# --------Creating a Contact Page-----------
 post "/contacts" do
   contact = Contact.create(
     first_name: params[:first_name],
-    last_name: params[:last_name],
-    email: params[:email],
-    note: params[:note]
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
   )
-  #creates new contact from user input
-  # Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-  # redirects you back to /contacts page
   redirect to ("/contacts")
 end
 
-
+# --------Viewing Specific Contact----------
 get "/contacts/:id/view" do
   @contact = Contact.find(params[:id].to_i)
   return erb(:show_contact)
 end
 
-
+# ---------Editing Contact----------
 get "/contacts/:id/edit" do
   @contact = Contact.find(params[:id].to_i)
   if @contact
@@ -57,7 +54,22 @@ get "/contacts/:id/edit" do
   end
 end
 
+# --------Editing Contact Page Input--------
+put "/contacts/:id" do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact.update({
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note],
+    })
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
 
+# ---------Deleting Contact----------
 get "/contacts/:id/delete" do
   contact = Contact.find(params[:id].to_i)
   if contact
